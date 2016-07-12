11536b06e43e5ae4383cee49fe01ad3b8ec7d70e
Jungmo <ajm100@ajou.ac.kr>
2016-07-12 16:34:07 +0900

    Add FILE IO code

--- src/makeTestingSet.py
+++ src/makeTestingSet.py
@@ -3,10 +3,10 @@ import GlobalVariable
 import cv2
 import numpy as np
 
-class TrainingSet:
-    imageList = os.listdir(GlobalVariable.trainingSetPath)
-    listPath = os.path.join(GlobalVariable.trainingSetPath, "list.txt")
-    labelPath = os.path.join(GlobalVariable.trainingSetPath, "label.txt")
+class TestingSet:
+    imageList = os.listdir(GlobalVariable.testSetPath)
+    listPath = os.path.join(GlobalVariable.testSetPath, "list.txt")
+    labelPath = os.path.join(GlobalVariable.testSetPath, "label.txt")
     imagePath = []
     numOfFile = 0
 
@@ -21,16 +21,16 @@ class TrainingSet:
             self.imagePath.append(line)
         self.numOfFile = len(self.imagePath[0])
 
-    def trX(self):
+    def teX(self):
         temp = []
         for i in range(0 , self.numOfFile):
-            path = os.path.join(GlobalVariable.trainingSetPath ,self.imagePath[0][i])
+            path = os.path.join(GlobalVariable.testSetPath, self.imagePath[0][i])
             image = cv2.imread(path, 1)
             temp.append(image)
             ret = np.array(temp)
         return ret
 
-    def trY(self):
+    def teY(self):
         f = open(self.labelPath,'r')
         ret = []
         for i in range(0, self.numOfFile):
